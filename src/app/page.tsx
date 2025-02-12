06f5dcb1bdbfa1fc05e83d9c9f06e47490f97d1b
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-02-12 19:56:17 +0530

    ADD: Added all the necessary shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant="destructive">Click me</Button>
+      <Button variant="destructive">Click me 123</Button>
     </div>
   );
 }
