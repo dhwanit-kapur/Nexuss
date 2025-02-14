72ca378c6aae38ba7dd7aa24d7f6f5e0fad6ccd2
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-02-14 17:29:04 +0530

    ADD: Created useRegister hook and mutation logic for sign up

--- src/app/features/auth/components/SignUpCard.tsx
+++ src/app/features/auth/components/SignUpCard.tsx
@@ -23,15 +23,14 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 
-const formSchema = z.object({
-  name: z.string().min(4, "Minimum of 4 characters"),
-  email: z.string().email(),
-  password: z.string().min(8, "Minimum of 8 characters"),
-});
+import { registerSchema } from "../schemas";
+import { useRegister } from "../api/useRegister";
 
 export const SignUpCard = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const { mutate } = useRegister();
+
+  const form = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
     defaultValues: {
       name: "",
       email: "",
@@ -39,8 +38,8 @@ export const SignUpCard = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const onSubmit = (values: z.infer<typeof registerSchema>) => {
+    mutate({ json: values });
   };
 
   return (
--- src/app/features/auth/schemas.ts
+++ src/app/features/auth/schemas.ts
@@ -4,3 +4,9 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8, "Minimum 8 characters"),
 });
+
+export const registerSchema = z.object({
+  name: z.string().min(4, "Minimum of 4 characters"),
+  email: z.string().email(),
+  password: z.string().min(8, "Minimum of 8 characters"),
+});
