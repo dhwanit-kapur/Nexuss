0273e33ce517efbed5a0a03722e7abc686541615
Dhwanit <dhwanitkapur.dk@gmail.com>
2025-02-16 22:13:28 +0530

    ADD: Created session middleware for auth and logout functionality

--- src/app/features/auth/server/route.ts
+++ src/app/features/auth/server/route.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 
 import { zValidator } from "@hono/zod-validator";
 import { createAdminClient } from "@/lib/appwrite";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 import { ID } from "node-appwrite";
 import { deleteCookie, setCookie } from "hono/cookie";
@@ -10,6 +11,11 @@ import { loginSchema, registerSchema } from "../schemas";
 import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
+  .get("/current", sessionMiddleware, (c) => {
+    const user = c.get("user");
+
+    return c.json({ data: user });
+  })
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
@@ -44,8 +50,12 @@ const app = new Hono()
 
     return c.json({ success: true });
   })
-  .post("/logout", (c) => {
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
+
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
+
     return c.json({ success: true });
   });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+import { useCurrent } from "./features/auth/api/useCurrent";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { useLogout } from "./features/auth/api/useLogout";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
-    <div className="flex gap-4">
-      <Button>Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="teritary">Teritary</Button>
+    <div>
+      <h2>Only visible to authorised users</h2>
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
